d48e9c0b9e674868e4e3f4db0e3938c2430bda4f
Benoit <benoit.dubus@outlook.fr>
2020-08-28 00:52:33 +0200

    discord bot - initialisation

---
+++ src/commands/misc/embed.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+  const embed = new MessageEmbed()
+    .setColor("#dc143c")
+    .setTitle("Titre de l'embed")
+    .setURL("https://www.google.com/")
+    .setDescription("Description de l'embed")
+    .setThumbnail(client.user.displayAvatarURL())
+    .addField("Je suis un champ", "je suis sa valeur", true)
+    .addFields(
+      {
+        name: "je suis le champ 1",
+        value: "et je suis sa valeur, je suis aligné",
+        inline: true,
+      },
+      {
+        name: "je suis le champ 2",
+        value: "et je suis une autre valeur",
+        inline: false,
+      }
+    )
+    .setImage(client.user.displayAvatarURL())
+    .setTimestamp()
+    .setFooter("le footer de l'embed");
+
+  message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: "embed",
+  aliases: ["embed"],
+  description: "Renvoie un embed.",
+  args: false,
+};
---
+++ src/commands/misc/userinfo.js
@@ -0,0 +1,15 @@
+module.exports.run = (client, message, args) => {
+  const user_mention = message.mentions.users.first() || message.author;
+  message.channel.send(
+    `Voici le tag de la personne mentionné : ${user_mention.tag}`
+  );
+};
+
+module.exports.help = {
+  name: "userinfo",
+  aliases: ["userinfo", "ui"],
+  description: "Renvoie les informatins d'un utiisateur mentionné.",
+  args: true,
+  usage: "<@utilisateur>",
+  cooldown: 10_000,
+};
