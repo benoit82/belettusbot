64b9ffa593a40fa540acdcdd8328ff9e5ca91bec
Benoit <benoit.dubus@outlook.fr>
2020-08-28 11:08:56 +0200

    clearlog cmd added

---
+++ src/commands/misc/clearlog.js
@@ -0,0 +1,56 @@
+const { PREFIX, TYPEINFO } = require("../../config");
+const { MessageCollector } = require("discord.js");
+const { logAction } = require("../../utils/botlog");
+
+module.exports.run = async (client, message, args) => {
+  if (args[0] === "all") {
+    const time = 5_000;
+    const collector = new MessageCollector(
+      message.channel,
+      (m) => m.author.id === message.author.id,
+      { time }
+    );
+    const cancelCmdConfirm = setTimeout(() => {
+      message.reply("Commande annulée");
+    }, time);
+
+    // confirm before clean all channel messages
+    message.reply(
+      `Es-tu certain de vouloir supprimer tous les messages du salon ?
+Réponds par \`oui\` sous ${
+        time / 1000
+      } secondes pour valider, sinon la commande sera annulée.`
+    );
+
+    collector.on("collect", async (msg) => {
+      if (msg.content === "oui") {
+        const messages = await message.channel.messages.fetch();
+        message.channel.bulkDelete(messages, true);
+        this.help.typeInfoLog = TYPEINFO.DANGER;
+        logAction(
+          client,
+          message,
+          this.help,
+          "Canal de discution totalement purgé."
+        );
+        clearTimeout(cancelCmdConfirm);
+      }
+    });
+  } else {
+    const messages = await message.channel.messages.fetch();
+    messages.forEach((message) => {
+      if (message.content.startsWith(PREFIX) || message.author.bot)
+        message.delete();
+    });
+    logAction(client, message, this.help, "Canal de discution nétoyé.");
+  }
+};
+
+module.exports.help = {
+  name: "clearlog",
+  aliases: ["clearlog"],
+  description:
+    "Nettoie le canal des commandes tapées et les réponses du bot.\nArgument optionel : `?clearlog all` pour effacer **TOUS** les messages du canal.",
+  args: false,
+  typeInfoLog: TYPEINFO.WARNING,
+};
