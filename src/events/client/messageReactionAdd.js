c150891ec918951645a9b8223bb4dcbdf5c32a52
Benoit <benoit.dubus@outlook.fr>
2020-09-03 11:38:45 +0200

    reaction management done

--- src/events/client/messageReactionAdd.js
+++ src/events/client/messageReactionAdd.js
@@ -1,4 +1,5 @@
 const { embedCreateFromEvent } = require("../../utils/functions");
+const moment = require("moment");
 
 module.exports = async (client, messageReaction, user) => {
   const { message } = messageReaction;
@@ -10,15 +11,35 @@ module.exports = async (client, messageReaction, user) => {
     try {
       await messageReaction.fetch();
     } catch (error) {
-      console.log(`probleme partial : ${error.message}`);
+      console.log(`problème partial : ${error.message}`);
     }
   }
-  if (eventTarget && eventTarget.isActive) {
-    const players = [...eventTarget.players, user.id];
-    // update eventTarget variable
-    eventTarget = await client.updateEvent(eventTarget, { players });
-    const embed = embedCreateFromEvent(client, message, eventTarget);
-    // update the event message
-    message.edit(embed);
+  if (eventTarget) {
+    if (
+      eventTarget.status === client.config.EVENT_STATUS.open &&
+      moment(eventTarget.rdv).isAfter(Date.now())
+    ) {
+      const players = [...eventTarget.players, user.id];
+      // update eventTarget variable
+      eventTarget = await client.updateEvent(eventTarget, { players });
+      const embed = embedCreateFromEvent(client, message, eventTarget);
+      // update the event message
+      message.edit(embed);
+    } else {
+      if (
+        eventTarget.status !== client.config.EVENT_STATUS.close &&
+        moment(eventTarget.rdv).isBefore(moment())
+      ) {
+        eventTarget = await client.updateEvent(eventTarget, {
+          status: client.config.EVENT_STATUS.close,
+        });
+      }
+      const embed = embedCreateFromEvent(client, message, eventTarget);
+      // update the event message
+      message.edit(embed);
+      // the event is canceled or event is before now -> remove all reaction
+      message.unpin({ reason: "évènement annulé ou dépassé." });
+      messageReaction.remove();
+    }
   }
 };
--- src/events/client/messageReactionRemove.js
+++ src/events/client/messageReactionRemove.js
@@ -1,4 +1,5 @@
 const { embedCreateFromEvent } = require("../../utils/functions");
+const moment = require("moment");
 
 module.exports = async (client, messageReaction, user) => {
   const { message } = messageReaction;
@@ -14,12 +15,32 @@ module.exports = async (client, messageReaction, user) => {
         console.log(`probleme partial : ${error.message}`);
       }
     }
-    let players = [...eventTarget.players];
-    const i = players.findIndex((el) => el === user.id);
-    players = players.filter((u, index) => index !== i);
-    eventTarget = await client.updateEvent(eventTarget, { players });
-    const embed = embedCreateFromEvent(client, message, eventTarget);
-    // update the event message
-    message.edit(embed);
+    if (
+      eventTarget.status === client.config.EVENT_STATUS.open &&
+      moment(eventTarget.rdv).isAfter(Date.now())
+    ) {
+      let players = [...eventTarget.players];
+      const i = players.findIndex((el) => el === user.id);
+      players = players.filter((u, index) => index !== i);
+      eventTarget = await client.updateEvent(eventTarget, { players });
+      const embed = embedCreateFromEvent(client, message, eventTarget);
+      // update the event message
+      message.edit(embed);
+    } else {
+      if (
+        eventTarget.status !== client.config.EVENT_STATUS.close &&
+        moment(eventTarget.rdv).isBefore(moment())
+      ) {
+        eventTarget = await client.updateEvent(eventTarget, {
+          status: client.config.EVENT_STATUS.close,
+        });
+      }
+      const embed = embedCreateFromEvent(client, message, eventTarget);
+      // update the event message
+      message.edit(embed);
+      // the event is canceled or event is before now -> remove all reaction
+      message.unpin({ reason: "évènement annulé ou dépassé." });
+      messageReaction.remove();
+    }
   }
 };
